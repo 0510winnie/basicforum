04e4ca8d4cc8e6b2607dde6e0d744fee66829da3
Luna <0510winnie@gmail.com>
2018-06-17 14:03:37 +0800

    finish navbar footer layouts

---
+++ resources/views/partials/_header.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link " id="logo" href=" {{ route('home') }}">Sample App</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Rigester</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.default')
+@section('title','Home')
+@section('content')
+<div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      From Here,
+    </p>
+    <p>
+      Set Sail Your Laravel Journey
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">Register Now</a>
+    </p>
+  </div>
+
+@endsection
